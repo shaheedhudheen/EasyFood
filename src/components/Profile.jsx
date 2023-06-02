d2f8315707e5566688df3396b56e5418af40a261
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2023-06-02 22:50:41 +0530

    changes were made

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Profile extends React.Component{
+ 
+    constructor (props){
+        super(props)
+
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Ahammed shaheedhudheen</h1>
+            </div>
+        )
+    }
+}
